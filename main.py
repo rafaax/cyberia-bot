332225ee2b44558aa067a743c19b39351a71d998
raphael <rafavfx1@gmail.com>
2024-02-15 22:33:26 -0300

    delete da classe

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import discord
 import random
-from Cyberia import Cyberia
 
-# This will load the permissions the bot has been granted in the previous configuration
 intents = discord.Intents.default()
 intents.message_content = True
+
+
+class Cyberia(discord.Client):
+    def __init__(self):
+        super().__init__(intents=intents)
+        self.synced = False
+        self.added = False
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync(guild=discord.Object(
+                '949532298007679008'))
+            self.synced = True
+        if not self.added:
+            self.added = True
+        print(f"Say hi to {self.user}!")
 
 
 client = Cyberia()
@@ -14,6 +29,7 @@ tree = discord.app_commands.CommandTree(client)
 @tree.command(description='teste por rafaax', guild=discord.Object('949532298007679008'))
 async def greet(interaction: discord.Interaction):
     await interaction.response.send_message('...')
+
 
 @tree.command(description='rola um dado', guild=discord.Object('949532298007679008'))
 async def d20(interaction: discord.Interaction):
